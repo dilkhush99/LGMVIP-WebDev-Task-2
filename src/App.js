a1f7926fa0e23ae2e325a7731b2ad6ee0ea1b3ec
Dilkhush Kumar <dilkhushraj05@gmail.com>
2021-09-21 00:33:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function App(){
+  const [users,setUsers] = useState();
+  const getUsers = async() => {
+    const response = await fetch("https://reqres.in/api/users?page=2"); 
+   
+    const data = await response.json();
+    setUsers(data.data);
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="navb">
+        <h1><a href="https://dilkhush99.github.io/LGMVIP-WebDev-Task-1/">eSafeTech</a></h1>
+        <div className="btn" onClick={getUsers}>
+          Get Users
+
+        </div>
+
       </header>
+      <div className="row">
+        {users?.map((current,index)=>{
+          return(
+            <div className="card column" key={index}>
+              <img src={current.avatar} className="card-img-top" alt=""/>
+              <div className="card-body">
+                <h4 className="card-title">{current.first_name} {current.last_name}</h4>
+                <h5 className="card-text">{current.email}</h5>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
